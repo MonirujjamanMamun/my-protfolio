59005fc347de02c7e7cf9a9f486477afed96d321
Monirujjaman Mamun <96871498+MonirujjamanMamun@users.noreply.github.com>
2022-06-09 19:07:37 +0600

    nave bare added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Navebar from './Pages/Shared/Navebar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import DataNotFound from './Pages/Shared/DataNotFound';
+import Projects from './Pages/Projects/Projects';
+import About from './Pages/About/About';
+import Blog from './Pages/Blog/Blog';
+import Contact from './Pages/Contact/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <h1>My Protfolio Web site for Web developer</h1>
+    <div>
+      <Navebar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/blog' element={<Blog />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/datanotfound' element={<DataNotFound />} />
+      </Routes>
     </div>
   );
 }
